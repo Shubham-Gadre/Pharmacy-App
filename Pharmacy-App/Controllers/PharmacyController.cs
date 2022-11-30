8e72e3c554a6d4c7912edfa17491859da6eaf9f3
Shubham Gadre <shubhamgadre@icloud.com>
2022-12-01 01:40:30 +0530

    Added few files for starting and testing

---
+++ Pharmacy-App/Controllers/PharmacyController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Pharmacy_App.Models;
+
+namespace Pharmacy_App.Controllers
+{
+    public class PharmacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetailPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewProduct(MedicinesViewModel _medicinetModel)
+        {
+
+            
+            if (ModelState.IsValid)
+            {
+               
+                //getproduct
+                //getproductbyid
+                //deleteproduct
+                //createproduct[Save]
+                //update/editproduct
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ Pharmacy-App/Models/MedicinesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy_App.Models
+{
+    public class MedicinesViewModel
+    {
+        public int MedicineId { get; set; } 
+
+        [Required]
+        public string MedicineName { get; set; }
+        [Required]
+        public string MedicineDescription { get; set; }
+        public int MedicinePrice { get; set; }
+        /*public IFormFile ProductImage { get; set; }
+        public string ProductImageUrl { get; set; }*/
+        public string? DosageForm { get; set; }
+        public string? MinimumOrderQuantity { get; set; }
+        public string? MedicineUsage { get; set; }
+        public string? MedicineManufacturedBy { get; set; }
+
+    }
+}
